26e60735ae1a22eec916833001185060be1fc099
Jordan <masson.jordan@gmail.com>
2018-04-18 15:19:39 +0200

    update

--- network.py
+++ network.py
@@ -5,7 +5,7 @@ from model import *
 import sys
 import socket
 import select
-
+import pickle
 ################################################################################
 #                          NETWORK SERVER CONTROLLER                           #
 ################################################################################
@@ -19,10 +19,17 @@ class NetworkServerController:
         self.s.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
         self.s.setblocking(False)
         self.s.bind(('',port))
-        self.s.listen(1)
+        self.s.listen(2)
         self.liste_socket = [self.s]
 
     # time event
+    def send_map(self, socket):
+        print("Sending map to: "+str(socket))
+        packet_to_send = pickle.dumps(self.model)
+        print("Will send: "+str(packet_to_send))
+        socket.send(packet_to_send)
+        print("Packet received: "+str(socket.recv(1500)))
+        print("Map well sent ! Hourra !")
         
     def tick(self, dt):
 	# creation of sockets + connexion
@@ -31,19 +38,17 @@ class NetworkServerController:
             if sock == self.s and lsock:
                 (con, ip) = self.s.accept() #on accepte un nouveau client aka nouvelle socket
                 print("test 1")
-                msg = joueur.recv(1000)
                 self.liste_socket.append(con)
+                self.model.add_character(con.recv(1500),True)
+                con.send(b'ACK')
             else :
-                print("nothing received")
                 try:
-                    recv_data = sock.recv(1500)
-                    if not recv_data:          #On test les données reçues
-                        self.liste_socket.remove(sock)
-                        sock.close()
-                        print("debug : le client est déconnecté")
-                        break
-                except:
-                    print("debug: except de fin")
+                    sock.recv(1500)
+                    self.send_map(sock)
+                except BrokenPipeError as e:
+                    print("Client disconnected: "+str(e))
+                    self.liste_socket.remove(sock)
+                
         return True
 
     
@@ -54,10 +59,31 @@ class NetworkServerController:
 class NetworkClientController:
 
     def __init__(self, file, host, port, nickname):
-        self.file = file
+        self.model = file
         self.host = host
         self.port = port
         self.nickname = nickname
+
+        self.s = socket.socket(family=socket.AF_INET6, type=socket.SOCK_STREAM, proto=0, fileno=None)
+        self.s.connect((host, port))
+        
+        self.s.send(b'nickname')
+        self.s.recv(1500)
+
+    def receive_map(self, host_socket):
+        print("Receiving map ...")
+        packet_received = host_socket.recv(2048)
+        print("My map is:   "+str(packet_received))
+        try:
+            packet_received = pickle.loads(packet_received)
+            #print("Packet = "+str(packet_received))
+        except EOFError:
+            print("OMG ERROR")
+            data = list()  # or whatever you want
+        print("   -> sending ACK")
+        host_socket.send(b'ACK')
+        print("Map well received")
+        return packet_received
 
     # keyboard events
 
@@ -78,7 +104,8 @@ class NetworkClientController:
     # time event
 
     def tick(self, dt):
-        # ...
+        self.s.send(b'ACK')
+        self.model = self.receive_map(self.s)
         return True
 '''
     # creating sockets
